c69a4942d060ca930ca4b268826f138b2c8f87cf
Heinrich Hartmann <derhein@gmail.com>
2024-02-21 21:52:28 +0000

    add flake

---
+++ nixos/etc/flake.nix
@@ -0,0 +1,17 @@
+{ config, lib, pkgs, ... }:
+
+{
+  description = "PVE Server Configuration";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+  outputs = { self, nixpkgs, ... }:
+    let
+      lib = nixpkgs.lib;
+    in
+      {
+        nixosConfigurations.pve = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [ ./configuration.nix ];
+        };
+}
