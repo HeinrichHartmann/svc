6030f2f7a3da36c1f85c5e0b736266f49af6fff7
Heinrich Hartmann <derhein@gmail.com>
2024-02-22 09:22:09 +0000

    .

---
+++ nixos/flake.nix
@@ -0,0 +1,12 @@
+{
+  description = "PVE Server Configuration";
+  inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11"; };
+  outputs = { self, nixpkgs, ... }:
+    let lib = nixpkgs.lib;
+    in {
+      nixosConfigurations.pve = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [ ./configuration.nix ];
+      };
+    };
+}
