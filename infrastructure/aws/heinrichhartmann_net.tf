e469e3d928a9bdef638a83e38f1db88207be87da
Heinrich Hartmann <heinrich.hartmann@zalando.de>
2025-04-23 17:49:41 +0200

    Refactor heinrichhartmann_net dns records

--- infrastructure/aws/heinrichhartmann_net.tf
+++ infrastructure/aws/heinrichhartmann_net.tf
@@ -1,102 +1,35 @@
+locals {
+  static_records = {
+    // Special Hosts
+    "gw.heinrichhartmann.net" = "192.168.2.1",
+    "smb.lan.heinrichhartmann.net" = "192.168.2.13",
+    "otel-collector.pve.heinrichhartmann.net" = "192.168.3.3",
+
+    // Root Domains
+    "*.heinrichhartmann.net" = "100.100.120.89", # PVE on tailscale network
+    "*.lan.heinrichhartmann.net" = "192.168.2.12", # PVE on LAN
+
+    // Host Records
+    "pve.ts.heinrichhartmann.net" = "100.100.120.89",
+    "*.pve.ts.heinrichhartmann.net" = "100.100.120.89",
+    "hifipi.ts.heinrichhartmann.net" = "100.97.166.55",
+    "*.hifipi.ts.heinrichhartmann.net" = "100.97.166.55",
+    "hhe15.ts.heinrichhartmann.net" = "100.121.201.109",
+    "*.hhe15.ts.heinrichhartmann.net" = "100.121.201.109",
+    "spohrhp20.ts.heinrichhartmann.net" = "100.67.92.4",
+    "*.spohrhp20.ts.heinrichhartmann.net" = "100.67.92.4"
+  }
+}
+
 resource "aws_route53_zone" "hhnet" {
   name    = "heinrichhartmann.net"
   comment = "local infrastructure"
 }
 
-resource "aws_route53_record" "gw" {
+resource "aws_route53_record" "static_records" {
+  for_each = local.static_records
   zone_id = aws_route53_zone.hhnet.zone_id
-  name    = "gw.heinrichhartmann.net"
-  type    = "A"
-  records = [
-    "192.168.2.1",
-  ]
-  ttl = 300
-}
-
-resource "aws_route53_record" "root" {
-  zone_id = aws_route53_zone.hhnet.zone_id
-  name    = "*.heinrichhartmann.net"
-  type    = "A"
-  records = [
-    "100.100.120.89",
-  ]
-  ttl = 300
-}
-
-resource "aws_route53_record" "lan" {
-  zone_id = aws_route53_zone.hhnet.zone_id
-  name    = "*.lan.heinrichhartmann.net"
-  type    = "A"
-  records = [
-    "192.168.2.12",
-  ]
-  ttl = 300
-}
-
-resource "aws_route53_record" "samba_lan" {
-  zone_id = aws_route53_zone.hhnet.zone_id
-  name    = "smb.lan.heinrichhartmann.net"
-  type    = "A"
-  records = [
-    "192.168.2.13",
-  ]
-  ttl = 300
-}
-
-resource "aws_route53_record" "otelcol_pve" {
-  zone_id = aws_route53_zone.hhnet.zone_id
-  name    = "otel-collector.pve.heinrichhartmann.net"
-  type    = "A"
-  records = [
-    "192.168.3.3",
-  ]
-  ttl = 300
-}
-
-resource "aws_route53_record" "tailscale" {
-  zone_id = aws_route53_zone.hhnet.zone_id
-  name    = "*.ts.heinrichhartmann.net"
-  type    = "A"
-  records = [
-    "100.100.120.89",
-  ]
-  ttl = 300
-}
-
-resource "aws_route53_record" "hifipi" {
-  zone_id = aws_route53_zone.hhnet.zone_id
-  name    = "*.hifipi.heinrichhartmann.net"
-  type    = "A"
-  records = [
-    "100.97.166.55",
-  ]
-  ttl = 300
-}
-
-locals {
-    ts_hosts = {
-        "pve" = "100.100.120.89"
-        "hifipi" = "100.97.166.55"
-        "hhe15" = "100.121.201.109"
-        "spohrhp20" = "100.67.92.4"
-    }
-}
-
-resource "aws_route53_record" "ts_hosts" {
-  for_each = local.ts_hosts
-  zone_id = aws_route53_zone.hhnet.zone_id
-  name    = "${each.key}.ts.heinrichhartmann.net"
-  type    = "A"
-  records = [
-    each.value,
-  ]
-  ttl = 300
-}
-
-resource "aws_route53_record" "ts_wildcards" {
-  for_each = local.ts_hosts
-  zone_id = aws_route53_zone.hhnet.zone_id
-  name    = "*.${each.key}.ts.heinrichhartmann.net"
+  name    = each.key
   type    = "A"
   records = [
     each.value,
