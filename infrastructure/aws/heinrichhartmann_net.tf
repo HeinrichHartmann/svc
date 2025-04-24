dcf26741f3d7353504f5c12668f7413a8b18fc3f
Heinrich Hartmann <heinrich.hartmann@zalando.de>
2025-04-24 14:10:11 +0200

    Cleanup hh.net hosts

--- infrastructure/aws/heinrichhartmann_net.tf
+++ infrastructure/aws/heinrichhartmann_net.tf
@@ -2,22 +2,19 @@ locals {
   static_records = {
     // Special Hosts
     "gw.heinrichhartmann.net" = "192.168.2.1",
-    "smb.lan.heinrichhartmann.net" = "192.168.2.13",
     "otel-collector.pve.heinrichhartmann.net" = "192.168.3.3",
 
-    // Root Domains
-    "*.heinrichhartmann.net" = "100.100.120.89", # PVE on tailscale network
-    "*.lan.heinrichhartmann.net" = "192.168.2.12", # PVE on LAN
+    // Home Network
+    "*.lan.heinrichhartmann.net" = "192.168.2.12", # pve on LAN
+    "pve.lan.heinrichhartmann.net" = "192.168.2.12",
+    "smb.lan.heinrichhartmann.net" = "192.168.2.13",
 
-    // Host Records
+    // Tailscale Network
+    "*.heinrichhartmann.net" = "100.100.120.89", # pve host
     "pve.ts.heinrichhartmann.net" = "100.100.120.89",
     "*.pve.ts.heinrichhartmann.net" = "100.100.120.89",
-    "hifipi.ts.heinrichhartmann.net" = "100.97.166.55",
-    "*.hifipi.ts.heinrichhartmann.net" = "100.97.166.55",
     "hhe15.ts.heinrichhartmann.net" = "100.121.201.109",
     "*.hhe15.ts.heinrichhartmann.net" = "100.121.201.109",
-    "spohrhp20.ts.heinrichhartmann.net" = "100.67.92.4",
-    "*.spohrhp20.ts.heinrichhartmann.net" = "100.67.92.4"
   }
 }
 
