539ee1b9dbc7c71d99622dc6a1e2a7bc8b9d5d21
Heinrich Hartmann <heinrich@heinrichhartmann.com>
2025-05-19 14:36:35 +0200

    .

--- infrastructure/letsencrypt/certbot.sh
+++ infrastructure/letsencrypt/certbot.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-poetry run certbot --work-dir=/svc/var/letsencrypt/work --logs-dir=/svc/var/letsencrypt/log --config-dir=/svc/var/letsencrypt/config $@
+poetry run certbot --work-dir=/share/hhartmann/var/letsencrypt/work --logs-dir=/share/hhartmann/var/letsencrypt/log --config-dir=/share/hhartmann/var/letsencrypt/config $@
